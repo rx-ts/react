fc326ecb059adf6aef01a71a68af777ef458fa02
JounQin <admin@1stg.me>
2020-02-03 21:29:43 +0800

    docs: consider .md files as mdx also

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,20 @@
 const { allowModules } = require('@1stg/eslint-config/_util')
+const { getGlobals } = require('eslint-plugin-mdx')
 
 module.exports = {
   extends: '@1stg',
   overrides: [
+    {
+      files: '*.md',
+      extends: 'plugin:mdx/overrides',
+      globals: getGlobals(['ReactQriousDemo', 'ReactQrcodeDemo']),
+      parserOptions: {
+        extensions: '.md',
+      },
+      rules: {
+        'react/jsx-no-target-blank': 0,
+      },
+    },
     {
       files: '**/docs/*.tsx',
       settings: {
