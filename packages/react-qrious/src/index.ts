caf03e0084ec6ea5202943b758e7aaa8268556f9
JounQin <admin@1stg.me>
2022-07-17 00:03:06 +0800

    fix(storage): compatibility on Node environment

--- packages/react-qrious/src/index.ts
+++ packages/react-qrious/src/index.ts
@@ -1,2 +1,5 @@
+/// <reference path="../qrious.d.ts" />
+/// <reference path="../typings.d.ts" />
+
 export * from './QRious.js'
 export * from './use-qrious.js'
---
+++ vitest.config.ts
@@ -0,0 +1,14 @@
+import autoImport from 'unplugin-auto-import/vite'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  plugins: [
+    autoImport({
+      imports: ['vitest'],
+      dts: true,
+    }),
+  ],
+  test: {
+    environment: 'happy-dom',
+  },
+})
